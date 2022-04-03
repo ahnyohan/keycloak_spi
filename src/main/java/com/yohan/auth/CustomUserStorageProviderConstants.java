d44eb3399255f1c425d83f1d4608d52b711ad070
ahnyohan <ayh0912@gmail.com>
2022-04-03 23:10:05 +0900

    init

---
+++ src/main/java/com/yohan/auth/CustomUserStorageProviderConstants.java
@@ -0,0 +1,9 @@
+package com.yohan.auth;
+
+public final class CustomUserStorageProviderConstants {
+    public static final String CONFIG_KEY_JDBC_DRIVER = "org.postgresql.Driver";
+    public static final String CONFIG_KEY_JDBC_URL = "jdbc:postgresql://192.168.0.39:5432/keycloak";
+    public static final String CONFIG_KEY_DB_USERNAME = "postgres";
+    public static final String CONFIG_KEY_DB_PASSWORD = "123qwe";
+    public static final String CONFIG_KEY_VALIDATION_QUERY = "SELECT VERSION()";
+}
